cf22d62c5e5ce82b82758f057a784678efdb0d4d
Azadev <kodingo593@gmail.com>
2024-09-09 17:42:27 +0400

    changes

--- src/components/header/LanguageSelector.tsx
+++ src/components/header/LanguageSelector.tsx
@@ -1,33 +1,49 @@
-import { useLang } from '../../context/SelectedLanguage'
-import { useScrolling } from '../../context/ScrollHeader';
-import { useLocation } from 'react-router-dom';
+import { useLang } from "../../context/SelectedLanguage";
+import { useScrolling } from "../../context/ScrollHeader";
+import { useLocation, useMatch } from "react-router-dom";
 
 const LanguageSelector = () => {
-
   const { selectedLanguage, handleSelectLanguage } = useLang();
   const { isScrolled } = useScrolling();
 
   const location = useLocation();
+  const match = useMatch("/blog_single/:blogid");
+  const match2 = useMatch("/news_single/:latestid");
+  const match3 = useMatch("/winner_single/:wthmid");
 
   return (
-    <div className={`language-selector ${location.pathname === "/blog" ? "lang-selector-blog" : ""}`}>
-     <select name="" id=""
-     style={{color:
-      location.pathname === "/whyride" ||
-      location.pathname === "/about" ||
-      location.pathname === "/contact" || isScrolled
-        ? "#000000"
-        : "#fff",
-      }}
-     onChange={(e) => handleSelectLanguage(e.target.value)}
-     value={selectedLanguage}
-     >
-          <option value="en">eng</option>
-          <option value="az">aze</option>
-          <option value="ru">rus</option>
-     </select>
-     </div>
-  )
-}
+    <div
+      className={`language-selector ${
+        location.pathname === "/blog"
+          ? "lang-selector-blog"
+          : match || match2 || match3
+          ? "lang-selector-blog-inner"
+          : location.pathname === "/bcycle"
+          ? "lang-item-bcycle"
+          : location.pathname === "/e-bcycle"
+          ? "lang-item-ebcycle"
+          : ""
+      }`}>
+      <select
+        name=""
+        id=""
+        style={{
+          color:
+            location.pathname === "/whyride" ||
+            location.pathname === "/about" ||
+            location.pathname === "/contact" ||
+            isScrolled
+              ? "#000000"
+              : "#fff",
+        }}
+        onChange={(e) => handleSelectLanguage(e.target.value)}
+        value={selectedLanguage}>
+        <option value="en">eng</option>
+        <option value="az">aze</option>
+        <option value="ru">rus</option>
+      </select>
+    </div>
+  );
+};
 
-export default LanguageSelector
+export default LanguageSelector;
--- src/components/header/UpScrollHeader.tsx
+++ src/components/header/UpScrollHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/header/upscrollheader.scss";
 import { useLogo } from "../../context/GetLogoContext";
 import { HeaderLogoType, HeaderTitleType } from "./Header";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useMatch } from "react-router-dom";
 import { useLang } from "../../context/SelectedLanguage";
 import { AccordingChangeColorToLocation } from "./ChangeHeaderLinkColor";
 import { useTranslateApi } from "../../context/GetTranslateContext";
@@ -11,114 +11,137 @@ import LanguageSelector from "./LanguageSelector";
 const UpScrollHeader = () => {
   const { logo } = useLogo();
   const { selectedLanguage } = useLang();
-  const { translatesWord } = useTranslateApi(); 
+  const { translatesWord } = useTranslateApi();
 
   const HeaderItem: HeaderTitleType[] = [
     {
       id: 1,
-      title: `${translatesWord['transport_nav']}`,
+      title: `${translatesWord["transport_nav"]}`,
       to: "/",
     },
     {
       id: 2,
-      title: `${translatesWord['why_ride_nav']}`,
+      title: `${translatesWord["why_ride_nav"]}`,
       to: "/whyride",
     },
     {
       id: 3,
-      title: `${translatesWord['blog_nav']}`,
+      title: `${translatesWord["blog_nav"]}`,
       to: "/blog",
     },
     {
       id: 4,
-      title: `${translatesWord['about_nav']}`,
+      title: `${translatesWord["about_nav"]}`,
       to: "/about",
     },
     {
       id: 5,
-      title: `${translatesWord['partner_nav']}`,
+      title: `${translatesWord["partner_nav"]}`,
       to: "/bepartner",
     },
     {
       id: 6,
-      title: `${translatesWord['contact_nav']}`,
+      title: `${translatesWord["contact_nav"]}`,
       to: "/contact",
     },
   ];
-  
 
   const location = useLocation();
 
-  const [dropdownVehicles, setDropdownVehicles] = React.useState<boolean>(false);
+  const [dropdownVehicles, setDropdown] = React.useState<number | null>(null);
+  const [hoveringMenu, setHoveringMenu] = React.useState<boolean>(false);
 
-  const vehiclesDivRef = React.useRef<HTMLDivElement | null>(null);
+  const handleDropdownMenu = (id: number | null) => {
+    setDropdown(id);
+  };
 
-  React.useEffect(() => {
-    const outsideClicked = (e:MouseEvent) => {
-      if(vehiclesDivRef.current && !vehiclesDivRef.current.contains(e.target as Node)) {
-        setDropdownVehicles(false)
-      } 
+  const handleMouseLeave = () => {
+    if (hoveringMenu) {
+      setDropdown(null);
     }
+  };
 
-    document.addEventListener('mousedown', outsideClicked);
-    return () => {
-      document.removeEventListener('click', outsideClicked);
-    }
-  }, []);
+  const match = useMatch("/blog_single/:blogid");
+  const match2 = useMatch("/news_single/:latestid");
+  const match3 = useMatch("/winner_single/:wthmid");
 
   return (
     <div className="up-scroll-header-wrapper">
       <div className="header-container-up-scroll">
-        
-
         <div className="bottom-fields-header">
-          <Link to="/" className="scrol-logo"
-          >
+          <Link to="/" className="scrol-logo">
             {logo.slice(1, 2).map((logo: HeaderLogoType, i: number) => (
               <img key={i} alt="scroll-logo-apar" src={logo.scroll_logo} />
             ))}
           </Link>
 
-            <div className="navleft">
-          <nav className="navbar">
-            {HeaderItem.map((item: HeaderTitleType, i: number) => (
-              <Link
-                to={item.to}
-                className={`header-item ${location.pathname === "/blog" ? "header-item-blog" : ""}`}
-                onClick={(e) => {
-                  item.id === 1 ? setDropdownVehicles((prev) => !prev) : ()=>{}
-                  item.id === 1  ? e.preventDefault() : ""
-                }}
-                style={{ color: item.id !== 1 ? AccordingChangeColorToLocation(item.id, location) : dropdownVehicles ? '#ff6600' : ''}}
-                key={i}>
-                {item.title}
-                {item.id === 1 && (
-                  <img src="../dovnblack.svg" 
-                  style={{transform: dropdownVehicles ? 'rotate(180deg)' : '', transition: '0.3s ease-out'}}
-                  alt="down-black" />
-                )}
-              </Link>
-            ))}
-            {dropdownVehicles && (
-              <div className="vehicles-dropdown" ref={vehiclesDivRef}>
-                <Link to='/e-bcycle' className="e-bcycle-title"
-                 style={{color: location.pathname === '/e-bcycle' ? '#ff6600' : ''}}
-                >
-                 {selectedLanguage === 'ru' ? 'Електронный велосипед' : 'E-bcycle'}
+          <div className="navleft">
+            <nav className="navbar">
+              {HeaderItem.map((item: HeaderTitleType, i: number) => (
+                <Link
+                  to={item.to}
+                  className={`header-item ${
+                    location.pathname === "/blog"
+                      ? "header-item-blog"
+                      : match || match2 || match3
+                      ? "bloginner"
+                      : location.pathname === "/bcycle"
+                      ? "header-item-bcycle"
+                      : location.pathname === "/e-bcycle"
+                      ? "header-item-ebcycle"
+                      : location.pathname === "/news_single" ? "header-item-news-single" : ""
+                  }`}
+                  onMouseEnter={() => {
+                    if (item.id === 1) {
+                      handleDropdownMenu(item?.id);
+                    }
+                  }}
+                  onMouseLeave={handleMouseLeave}
+                  style={{
+                    color:
+                      item.id !== 1
+                        ? AccordingChangeColorToLocation(item.id, location)
+                        : dropdownVehicles
+                        ? "#ff6600"
+                        : "",
+                  }}
+                  key={i}>
+                  {item.title}
+                  {item.id === 1 && (
+                    <img
+                      src="../dovnblack.svg"
+                      style={{ transform: dropdownVehicles ? "rotate(180deg)" : "", transition: "0.3s ease-out" }}
+                      alt="down-black"
+                    />
+                  )}
                 </Link>
-                <Link to='/bcycle' className="bcycle-title"
-                 style={{color: location.pathname === '/bcycle' ? '#ff6600' : ''}}
-                >
-                {selectedLanguage === 'ru' ? 'Велосипед' : 'Bcycle'}
-                </Link>
-              </div>
-            )}
-          </nav>
+              ))}
+              {dropdownVehicles && (
+                <div
+                  className="vehicles-dropdown"
+                  onMouseEnter={() => setHoveringMenu(true)}
+                  onMouseLeave={() => {
+                    setHoveringMenu(false);
+                    setDropdown(null);
+                  }}>
+                  <Link
+                    to="/e-bcycle"
+                    className="e-bcycle-title"
+                    style={{ color: location.pathname === "/e-bcycle" ? "#ff6600" : "" }}>
+                    {selectedLanguage === "ru" ? "Електронный велосипед" : "E-bcycle"}
+                  </Link>
+                  <Link
+                    to="/bcycle"
+                    className="bcycle-title"
+                    style={{ color: location.pathname === "/bcycle" ? "#ff6600" : "" }}>
+                    {selectedLanguage === "ru" ? "Велосипед" : "Bcycle"}
+                  </Link>
+                </div>
+              )}
+            </nav>
 
-<LanguageSelector />
-            </div>
-
-          
+            <LanguageSelector />
+          </div>
         </div>
       </div>
     </div>
